51014f2018a6ae3a2c597857f6e4ca4357c3211b
Edinson Nuñez More <nmedinson@gmail.com>
2021-03-09 00:22:25 -0500

    fix eslint

--- src/pacificoseguros-spotify-styleguide.tsx
+++ src/pacificoseguros-spotify-styleguide.tsx
@@ -1,4 +1,4 @@
 export { default as Button } from "./components/button";
 export * from "./components/full-screen-loader/full-screen-loader";
 // export { default as Logo } from "./components/icons/logo";
-export * from './components/icons';
+export * from "./components/icons";
